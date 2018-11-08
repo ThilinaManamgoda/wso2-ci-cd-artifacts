dadbec1a1932caa37e24ddf01f221603d2b7eb86
VimukthiPerera <vimukthi@wso2.com>
2018-11-08 15:58:43 +0530

    Parameterize the TEST_URL

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CODE=`wget --server-response http://localhost:8280/services/EchoProxy 2>&1 | awk '/^  HTTP/{print $2}'`;
+CODE=`wget --server-response http://${TEST_URL}/services/EchoProxy 2>&1 | awk '/^  HTTP/{print $2}'`;
 [ ${CODE} -eq "200" ] && exit 0 || exit 1
